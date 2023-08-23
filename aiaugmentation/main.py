39cb2bc89ba13f10298ce4cc817154c3565e19d8
Digging Worm <Robin.Korfmann@Student.Reutlingen-university.DE>
2023-08-23 17:27:00 +0200

    Testing gpt implementation

--- aiaugmentation/main.py
+++ aiaugmentation/main.py
@@ -31,14 +31,15 @@ print("test")
 # write.write_file(data.delete_duplicates_txt(rttru.execute_rtt(read.read_raw_data(DATA), False)), "RTTen2ru")
 # write.write_json(data.delete_duplicates_json(rttru.execute_rtt(read.read_raw_data(DATA), True)), "RTTen2ru")
 
-# write.write_file(data.delete_duplicates_txt(gen.execute_gpt(EXAMPLE_PROMPT, 20, False)), "GPT")
-# write.write_json(data.delete_duplicates_json(gen.execute_gpt(EXAMPLE_PROMPT, 20, True)), "GPT")
+gpt.gpt(read.read_raw_data(["Test.txt"]))
+write.write_file(data.delete_duplicates_txt(gen.execute_gpt(EXAMPLE_PROMPT, 20, False)), "GPT")
+write.write_json(data.delete_duplicates_json(gen.execute_gpt(EXAMPLE_PROMPT, 20, True)), "GPT")
 
-write.write_file(data.delete_duplicates_txt(eda.execute_eda(read.read_raw_data(["Test.txt"],), False)), "EDA")
-write.write_json(data.delete_duplicates_json(eda.execute_eda(read.read_raw_data(["Test.txt"],), True)), "EDA")
+# write.write_file(data.delete_duplicates_txt(eda.execute_eda(read.read_raw_data(["Test.txt"],), False)), "EDA")
+# write.write_json(data.delete_duplicates_json(eda.execute_eda(read.read_raw_data(["Test.txt"],), True)), "EDA")
 
-write.write_file(data.delete_duplicates_txt(pa.execute_para(read.read_raw_data(["Test.txt"],), False)), "PARA")
-write.write_json(data.delete_duplicates_json(pa.execute_para(read.read_raw_data(["Test.txt"],), True)), "PARA") 
+# write.write_file(data.delete_duplicates_txt(pa.execute_para(read.read_raw_data(["Test.txt"],), False)), "PARA")
+# write.write_json(data.delete_duplicates_json(pa.execute_para(read.read_raw_data(["Test.txt"],), True)), "PARA") 
 
 #-----------------------------------------------------------------------------------------------------
 #------------------------------------------Train AI Models--------------------------------------------
