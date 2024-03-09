7cea8f6e47071437a7f508baff7bc7d25a659f94
Robin Korfmann <Robin.Korfmann@Student.Reutlingen-University.DE>
2024-03-09 14:59:49 +0100

    fixed read_raw_data

--- aiaugmentation/main.py
+++ aiaugmentation/main.py
@@ -6,7 +6,7 @@ import data.write as write
 import data.read as read
 import eda.eda as eda
 # import rtt.rttru as rttru
-# import rtt.rttde as rttde
+import rtt.rttde as rttde
 import para.paraphrase as pa
 # import experiment as ex
 import evaluate
@@ -18,12 +18,10 @@ TRAINING_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),"data","i
 RESULT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),"output")
 
 # write.write_file(data.delete_duplicates_txt(rttru.execute_rtt(read.read_raw_data(DATA), False)), "RTTen2ru")
-"""write.write_json(data.delete_duplicates_json(rttde.execute_rtt(read.read_raw_data(["demo.txt"]), True)), "RTTen2de")
+write.write_json(data.delete_duplicates_json(rttde.execute_rtt(read.read_raw_data(["demo.txt"]), True)), "RTTen2de")
 write.write_json(data.delete_duplicates_json(pa.execute_para(read.read_raw_data(["demo.txt"]), True)), "PARA") 
 write.write_json(data.delete_duplicates_json(rttde.execute_rtt(read.read_raw_data(["demo.txt"]), True)), "EDA")
-write.write_file(data.delete_duplicates_json(pa.execute_para(read.read_raw_data(["demo.txt"]), True)), "GPT") """
-
-read.read_raw_data(["demo.txt"])
+write.write_file(data.delete_duplicates_json(pa.execute_para(read.read_raw_data(["demo.txt"]), True)), "GPT")
 
 """i = 0
 for r in RESULTS:
